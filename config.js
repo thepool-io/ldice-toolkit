53e9fc4ef5027f5221c18ea8769d031d5a267aef
karek314 <karek314@protonmail.ch>
2020-02-13 22:29:11 +0800

    date fix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.server = "http://127.0.0.1:4000";
 config.genesisPayloadHash = "304b5c4b96ff6ff1230689c25db983a63a077b91cc881d18042d1f333ba65662";
 config.networkId = "ldice";
 config.senderAccount = "";
-config.startDate = Date.parse(new Date(Date.UTC(2019, 4, 24, 17, 0, 0, 0)).toISOString());
+config.startDate = new Date(Date.UTC(2020, 2, 11, 17, 0, 0, 0)).toISOString();
 
 config.numberOfTransactions = 25;
 config.numberOfChunks = 25;
